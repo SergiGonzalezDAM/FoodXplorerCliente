120c83ebf2e6278ca254834d643b3d7f9cf0f384
AbelSerranoDAM <abeleitor6@gmail.com>
2017-05-24 19:11:12 +0200

    COSAS HECHAS

--- app/src/main/java/com/foodxplorer/foodxplorer/fragments/FragmentResumenPedido.java
+++ app/src/main/java/com/foodxplorer/foodxplorer/fragments/FragmentResumenPedido.java
@@ -13,12 +13,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.foodxplorer.foodxplorer.Direccion;
+import com.foodxplorer.foodxplorer.LineasPedido;
 import com.foodxplorer.foodxplorer.MainActivity;
 import com.foodxplorer.foodxplorer.Pedidos;
 import com.foodxplorer.foodxplorer.R;
 import com.foodxplorer.foodxplorer.helpers.AsyncResponse;
 import com.foodxplorer.foodxplorer.helpers.Settings;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,6 +29,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -37,8 +40,10 @@ import static com.foodxplorer.foodxplorer.helpers.Settings.LOGTAG;
 public class FragmentResumenPedido extends Fragment {
     private MainActivity tienda;
     private TextView nombreCliente, importe, direccion, estado;
+    private double importeTotal;
     private Pedidos pedido;
     private Direccion direccionObject;
+    private ArrayList<LineasPedido> listaLineasPedido;
 
     public FragmentResumenPedido() {
         // Required empty public constructor
@@ -61,6 +66,8 @@ public class FragmentResumenPedido extends Fragment {
         nombreCliente.setText(tienda.CurrentState.getUsuarioLogueado());
         TareaWSRecuperarDireccion tareaRecDir = new TareaWSRecuperarDireccion();
         tareaRecDir.execute();
+        TareaWSRecuperarLineasPedido tareaLines = new TareaWSRecuperarLineasPedido();
+        tareaLines.execute();
         List<String> lista = new ArrayList();
         lista.add("Barbacoa");
         lista.add("4 Quesos");
@@ -72,7 +79,6 @@ public class FragmentResumenPedido extends Fragment {
 
     class TareaWSRecuperarDireccion extends AsyncTask<Object, Void, Boolean> {
         JSONObject direccionJSON;
-        public AsyncResponse delegate = null;
 
         @Override
         protected Boolean doInBackground(Object... params) {
@@ -139,13 +145,17 @@ public class FragmentResumenPedido extends Fragment {
     }
 
     class TareaWSRecuperarLineasPedido extends AsyncTask<Object, Void, Boolean> {
-        JSONObject lineasPedidoJSON;
-        public AsyncResponse delegate = null;
+        JSONArray lineasPedidoJSON;
 
         @Override
         protected Boolean doInBackground(Object... params) {
+            BufferedReader reader;
+            URL url = null;
             try {
-                lineasPedidoJSON = readJsonFromUrl(Settings.DIRECCIO_SERVIDOR + "ServcioFoodXPlorer/webresources/generic/direccion/obtener2/" + pedido.getIdDireccion());
+                url = new URL(Settings.DIRECCIO_SERVIDOR + "ServcioFoodXPlorer/webresources/generic/pedido/obtenerDetalles/" + pedido.getIdPedido());
+                reader = getBufferedReader(url);
+                lineasPedidoJSON = new JSONArray(reader.readLine());
+
             } catch (java.io.FileNotFoundException ex) {
                 Log.e(LOGTAG, "Error al obtener la direccion");
             } catch (java.io.IOException ex) {
@@ -156,36 +166,26 @@ public class FragmentResumenPedido extends Fragment {
             return true;
         }
 
-
-        private String readAll(Reader rd) throws IOException {
-            StringBuilder sb = new StringBuilder();
-            int cp;
-            while ((cp = rd.read()) != -1) {
-                sb.append((char) cp);
-            }
-            return sb.toString();
-        }
-
-        private JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-            InputStream is = new URL(url).openStream();
-            try {
-                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-                String jsonText = readAll(rd);
-                JSONObject json = new JSONObject(jsonText);
-                return json;
-            } finally {
-                is.close();
-            }
+        private BufferedReader getBufferedReader(URL url) throws java.io.IOException {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            // conn.setReadTimeout(10000 /*milliseconds*/);
+            // conn.setConnectTimeout(10000);
+            conn.setRequestProperty("Content-Type", "application/json");
+            return new BufferedReader(new InputStreamReader(conn.getInputStream()));
         }
 
         @Override
         protected void onPostExecute(Boolean result) {
             if (result) {
                 try {
-                    if (!rellenarObjeto() || tienda.CurrentState.getUsuarioLogueado() == null || tienda.CurrentState.getUsuarioLogueado().equals("")) {
-                        Toast.makeText(tienda, "NO EXISTE ESE NUMERO DE PEDIDO", Toast.LENGTH_SHORT).show();
+                    if (!rellenarArray() || tienda.CurrentState.getUsuarioLogueado() == null || tienda.CurrentState.getUsuarioLogueado().equals("")) {
+                        Toast.makeText(tienda, "ERROR", Toast.LENGTH_SHORT).show();
                     } else {
-                        direccion.setText(direccionObject.getPiso() + "\n" + direccionObject.getCalle() + "\n" + direccionObject.getPoblacion());
+                        for (LineasPedido linea : listaLineasPedido) {
+                            importeTotal += linea.getPrecio();
+                        }
+                        importe.setText(String.valueOf(importeTotal) + "€");
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -193,11 +193,16 @@ public class FragmentResumenPedido extends Fragment {
             }
         }
 
-        private boolean rellenarObjeto() throws JSONException {
+        private boolean rellenarArray() throws JSONException {
             boolean estado;
-            if (lineasPedidoJSON != null) {
-                direccionObject = new Direccion(lineasPedidoJSON.getLong("idDireccion"), lineasPedidoJSON.getString("calle"), lineasPedidoJSON.getString("piso"),
-                        lineasPedidoJSON.getString("poblacion"), lineasPedidoJSON.getString("codPostal"));
+            listaLineasPedido = new ArrayList<>();
+            if (lineasPedidoJSON.length() > 0) {
+                for (int i = 0; i < lineasPedidoJSON.length(); i++) {
+                    JSONObject jsonobject = lineasPedidoJSON.getJSONObject(i);
+                    LineasPedido lineasPedido = new LineasPedido(jsonobject.getLong("idPedido"), jsonobject.getLong("idProducto"), jsonobject.getInt("cantidad"),
+                            jsonobject.getDouble("precio"), jsonobject.getInt("iva"));
+                    listaLineasPedido.add(lineasPedido);
+                }
                 estado = true;
             } else {
                 estado = false;
