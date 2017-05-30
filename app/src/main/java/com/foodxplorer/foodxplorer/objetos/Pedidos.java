a1b400398e26f9100656b0abe7a09bf08a18c50b
SergiGonzalezDAM <job_sergi@hotmail.es>
2017-05-30 17:33:51 +0200

    Errores?

--- app/src/main/java/com/foodxplorer/foodxplorer/objetos/Pedidos.java
+++ app/src/main/java/com/foodxplorer/foodxplorer/objetos/Pedidos.java
@@ -1,10 +1,5 @@
 package com.foodxplorer.foodxplorer.objetos;
 
-import android.graphics.drawable.Drawable;
-
-/**
- * Created by ALUMNEDAM on 15/05/2017.
- */
 
 public class Pedidos {
     private long idPedido;
@@ -12,6 +7,10 @@ public class Pedidos {
     private long idDireccion;
     private long idEstado;
 
+    /**
+     * Generamos la clase Pedidos con un constructor vacio y otro con los datos necesarios para poder recuperarlos de la base de datos
+     * lo usaremos básicamente para obtener todos los pedidos de un usuario
+     */
     public Pedidos() {
     }
 
@@ -26,31 +25,16 @@ public class Pedidos {
         return fechaPedido;
     }
 
-    public void setFechaPedido(String fechaPedido) {
-        this.fechaPedido = fechaPedido;
-    }
-
     public long getIdPedido() {
         return idPedido;
-    }
-
-    public void setIdPedido(long idPedido) {
-        this.idPedido = idPedido;
     }
 
     public long getIdDireccion() {
         return idDireccion;
     }
 
-    public void setIdDireccion(long idDireccion) {
-        this.idDireccion = idDireccion;
-    }
-
     public long getIdEstado() {
         return idEstado;
     }
 
-    public void setIdEstado(long idEstado) {
-        this.idEstado = idEstado;
-    }
 }
