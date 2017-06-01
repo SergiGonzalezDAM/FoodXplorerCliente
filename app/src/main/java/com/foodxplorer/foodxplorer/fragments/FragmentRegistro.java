551cea3130e252cd069df9b861b34b084b464147
Roger G. Coscojuela <rogilico@gmail.com>
2017-06-01 19:53:46 +0200

    refactor

--- app/src/main/java/com/foodxplorer/foodxplorer/fragments/FragmentRegistro.java
+++ app/src/main/java/com/foodxplorer/foodxplorer/fragments/FragmentRegistro.java
@@ -2,9 +2,9 @@ package com.foodxplorer.foodxplorer.fragments;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +16,7 @@ import android.widget.EditText;
 
 import com.foodxplorer.foodxplorer.MainActivity;
 import com.foodxplorer.foodxplorer.R;
+import com.foodxplorer.foodxplorer.helpers.MD5;
 import com.foodxplorer.foodxplorer.helpers.Settings;
 
 import org.json.JSONException;
@@ -101,7 +102,8 @@ public class FragmentRegistro extends Fragment implements View.OnClickListener {
         private String getStringJSON(Object... params) throws JSONException, UnsupportedEncodingException {
             JSONObject dato = new JSONObject();
             dato.put("correo", params[0]);
-            dato.put("contrasena", params[1]);
+            MD5 md5cypher = new MD5();
+            dato.put("contrasena", md5cypher.md5((String) params[1]));
             Log.d(LOGTAG, "El usuario que se insertara es:" + dato.toString());
             return String.valueOf(dato);
         }
---
+++ app/src/main/java/com/foodxplorer/foodxplorer/helpers/MD5.java
@@ -0,0 +1,33 @@
+package com.foodxplorer.foodxplorer.helpers;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Projecte FoodXplorer
+ *
+ * @Autors Abel Serrano, Sergi Gonazalez, Roger G.
+ * Created by IES on 01/06/2017.
+ */
+
+public class MD5 {
+
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
